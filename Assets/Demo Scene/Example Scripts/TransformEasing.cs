4d9c74ace1e7052a806cd4343d3b36de0484b733
Ma_Bna_Ur <modernalchemyapps@gmail.com>
2017-04-17 06:53:08 +0200

    inital commit

---
+++ Assets/Demo Scene/Example Scripts/TransformEasing.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SimpleEasing;
+
+public class TransformEasing : MonoBehaviour {
+	public Transform easeTarget;
+	public float length;
+	public Vector3 target;
+	public EasingTypes easing;
+	//So that ther is only one tween active on the transform
+	private UniqueCoroutine uniqueAnimation = new UniqueCoroutine();
+	
+	public void EaseScale()
+	{
+		//So that the tween won't be interfere with a previous tween
+		uniqueAnimation.ReplaceOrStartTween(
+			easeTarget.ScaleTo(target, length, easing)
+		);
+	}
+	public void EasePosition()
+	{
+		uniqueAnimation.ReplaceOrStartTween(
+			easeTarget.MoveTo(target, length, easing)
+		);
+	}
+	public void EaseRotation()
+	{
+		uniqueAnimation.ReplaceOrStartTween(
+			easeTarget.RotateTo(target, length, easing)
+		);
+	}
+
+	public void SetLength(string time)
+	{
+		if(string.IsNullOrEmpty(time))
+			length = 0f;
+		else
+			length = float.Parse(time);
+	}
+
+	public void SetX(string x)
+	{
+		if(string.IsNullOrEmpty(x))
+			target.x = 0f;
+		else
+			target.x = float.Parse(x);
+	}
+
+	public void SetY(string y)
+	{
+		if(string.IsNullOrEmpty(y))
+			target.y = 0f;
+		else
+			target.y = float.Parse(y);
+	}
+
+	public void SetZ(string z)
+	{
+		if(string.IsNullOrEmpty(z))
+			target.z = 0f;
+		else
+			target.z = float.Parse(z);
+	}
+
+	public void SetEasing(int enumIndex)
+	{
+		easing = (EasingTypes)enumIndex;
+		print(enumIndex);
+		print(easing);
+	}
+}
---
+++ Assets/Easing/TweenRepeat.cs
@@ -0,0 +1,10 @@
+namespace SimpleEasing
+{	
+using UnityEngine;
+using System.Collections;
+
+	public enum TweenRepeat
+	{
+		Once, PingPong, Loop
+	}
+}
